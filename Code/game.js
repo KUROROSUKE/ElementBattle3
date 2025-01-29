59d640ccbdc5ec48e57d6edc994d62417cf069e2
KUROROSUKE <161828247+KUROROSUKE@users.noreply.github.com>
2025-01-29 17:10:20 +0900

    Update game.js

--- Code/game.js
+++ Code/game.js
@@ -8,7 +8,7 @@ const WIN_TURN = 10
 
 let droped_cards_p1 = []; let droped_cards_p2 = []
 
-let turn = "p2"
+let turn = "p1"
 let time = "game"
 let numTurn = 1
 
@@ -35,51 +35,44 @@ async function view_p2_hand() {
     p2_hand.forEach((elem, index) => {
         const image = document.createElement("img")
         image.src = `../images/${elementToNumber[elem]}.png`
-        image.name = index
         image.alt = elem
-        image.style.width = `${-10+WindowSize/12}px`
+        image.style.width = `${WindowSize/12}px`
         image.style.padding = "10px"
         image.style.margin = "5px"
         image.style.border = "1px solid #000"
         image.classList.add("selected")
         image.addEventListener("click", function() {
-            if (turn == "p2") {
-                if (time == "make") {
-                    this.classList.toggle("selected")
-                    if (this.classList.contains("selected")){
-                        this.style.border = "1px solid #000"
-                        this.style.padding = "10px"
-                        p2_selected_card.splice(p2_selected_card.indexOf(this.alt),1)
-                    } else {
-                        this.style.border = "5px solid #F00"
-                        this.style.padding = "6px"
-                        p2_selected_card.push(this.alt)
-                    }}
-                if (time == "game") {
-                    droped_cards_p2.push(this.alt)
-                    const img = document.createElement("img")
-                    console.log(this.alt)
-                    img.alt = this.alt
-                    img.src=`../images/${elementToNumber[this.alt]}.png`
-                    img.style.width = `${-10+WindowSize/24}px`
-                    img.style.border = "1px solid #000"
-                    document.getElementById("droped_area_p2").appendChild(img)
-                    this.classList.remove("selected")
-                    this.classList.add("selected")
-                    let newElem = elements[Math.round(Math.random()*elements.length)]
-                    this.src = `../images/${elementToNumber[newElem]}.png`
-                    document.getElementById("p1_explain").innerHTML = `newElem:${this.newElem}, index: ${index}`
-                    this.alt = newElem
-                    this.style.width = `${-10+WindowSize/12}px`
-                    this.style.padding = "10px"
-                    this.style.margin = "5px"
+            if (time == "make") {
+                this.classList.toggle("selected")
+                if (this.classList.contains("selected")){
                     this.style.border = "1px solid #000"
-                    console.log(p2_hand)
-                    p2_hand[this.name] = newElem
-                    console.log(p2_hand)
-                    turn = "p1"
-                    setTimeout(() => {p1_exchange()},500)
-                }
+                    this.style.padding = "10px"
+                    p2_selected_card.splice(p2_selected_card.indexOf(this.alt),1)
+                } else {
+                    this.style.border = "5px solid #F00"
+                    this.style.padding = "6px"
+                    p2_selected_card.push(this.alt)
+                }}
+            if (turn == "p2" && time == "game") {
+                droped_cards_p2.push(this.alt)
+                const img = document.createElement("img")
+                img.alt = this.alt
+                img.src=`../images/${elementToNumber[this.alt]}.png`
+                img.style.width = `${WindowSize/24}px`
+                img.style.border = "1px solid #000"
+                document.getElementById("droped_area_p2").appendChild(img)
+                this.classList.remove("selected")
+                this.classList.add("selected")
+                let newElem = elements[Math.floor(Math.random()*(elements.length-1))]
+                this.src = `../images/${elementToNumber[newElem]}.png`
+                this.alt = newElem
+                this.style.width = `${WindowSize/12}px`
+                this.style.padding = "10px"
+                this.style.margin = "5px"
+                this.style.border = "1px solid #000"
+                p2_hand[index] = newElem
+                turn = "p1"
+                setTimeout(() => {p1_exchange()},500)
             }
         })
         area.appendChild(image)
@@ -92,7 +85,7 @@ async function view_p1_hand() {
         const image = document.createElement("img")
         image.src = `../images/0.png`
         image.alt = elem
-        image.style.width = `${-10+WindowSize/12}px`
+        image.style.width = `${WindowSize/12}px`
         image.style.padding = "10px"
         image.style.margin = "5px"
         image.style.border = "1px solid #000"
@@ -118,16 +111,8 @@ async function search(components) {
     })
 }
 
-document.getElementById("done_button").addEventListener("click", function () {
-    console.log("run")
-    if (turn == "p2") {
-        time = "make"
-        done()
-    }
-})
-
 async function p1_make() {
-    //console.log("P2はアガるための元素を選んでください。")
+    //console.log("P1はアガるための元素を選んでください。")
     //FIXME:ここに上がるための元素を選択するコードを実装（相手の元素の読みなどを含めて）
 
     //return [返す元素のデータ]
@@ -135,13 +120,15 @@ async function p1_make() {
 }
 
 async function p2_make() {
-    console.log("P1はアガるための元素を選んでください。")
+    // ボタンの表示を変更
+    document.getElementById("generate_button").style.display = "none"
+    const button = document.getElementById("done_button")
+    button.style.display = "inline"
     // ボタンクリックを待機
     return new Promise((resolve) => {
-        document.getElementById("done_button").addEventListener("click", function () {
-            const p1_make_material = search(arrayToObj(p1_selected_card))
-            console.log(p1_make_material)
-            resolve(p1_make_material) // ボタンクリック後にデータを返す
+        button.addEventListener("click", function () {
+            const p2_make_material = search(arrayToObj(p2_selected_card))
+            resolve(p2_make_material) // ボタンクリック後にデータを返す
         })
     })
 }
@@ -153,38 +140,45 @@ async function get_dora() {
 async function done() {
     const p2_make_material = await p2_make()
     const p1_make_material = await p1_make()
-    console.log(p1_make_material[0])
+    console.log(p2_make_material[0])
     dora = await get_dora()
     console.log(`dora: ${dora}`)
-    let thisGame_p1_point = p1_make_material[0].point
-    let thisGame_p2_point = p2_make_material.point
+    let thisGame_p2_point = p2_make_material[0].point
+    let thisGame_p1_point = p1_make_material.point
     // if you're material has advantage, you get more point
-    if (Boolean(p1_make_material[0].advantageous.includes(p2_make_material.formula))) {
-        thisGame_p1_point = thisGame_p1_point*(1.5+Math.random()/2)
-    } else if (Boolean(p2_make_material.advantageous.includes(p1_make_material[0].formula))) {
+    if (Boolean(p2_make_material[0].advantageous.includes(p1_make_material.formula))) {
         thisGame_p2_point = thisGame_p2_point*(1.5+Math.random()/2)
+    } else if (Boolean(p1_make_material.advantageous.includes(p2_make_material[0].formula))) {
+        thisGame_p1_point = thisGame_p1_point*(1.5+Math.random()/2)
     }
     // if you're material's elements include dora, you get more point
-    if (Boolean(Object.keys(p1_make_material[0].components).includes(dora))) {
-        thisGame_p1_point = thisGame_p1_point*1.5
-    } else if (Boolean(Object.keys(p2_make_material.components).includes(dora))) {
+    if (Boolean(Object.keys(p2_make_material[0].components).includes(dora))) {
         thisGame_p2_point = thisGame_p2_point*1.5
+    } else if (Boolean(Object.keys(p1_make_material.components).includes(dora))) {
+        thisGame_p1_point = thisGame_p1_point*1.5
     }
     // after multipulication, the value often float number, so round to integer.
-    p1_point += Math.round(thisGame_p1_point)
     p2_point += Math.round(thisGame_p2_point)
-    document.getElementById("p1_point").innerHTML = `ポイント：${p1_point}`
+    p1_point += Math.round(thisGame_p1_point)
     document.getElementById("p2_point").innerHTML = `ポイント：${p2_point}`
-    document.getElementById("p1_explain").innerHTML = `生成物質：${p1_make_material[0].name}`
-    document.getElementById("p2_explain").innerHTML = `生成物質：${p2_make_material.name}`
+    document.getElementById("p1_point").innerHTML = `ポイント：${p1_point}`
+    document.getElementById("p2_explain").innerHTML = `生成物質：${p2_make_material[0].name}`
+    document.getElementById("p1_explain").innerHTML = `生成物質：${p1_make_material.name}`
     // win check.
     winner = await win_check()
-    console.log(winner)
+    console.log(`winner: ${winner} (if no winner, then this properties are show "null".)`)
+    document.getElementById("done_button").style.display = "none"
+    const button = document.getElementById("nextButton")
+    button.style.display = "inline"
     if (!winner) {
         console.log("次のゲーム")
+        numTurn += 1
+        button.textContent = "次のゲーム"
+        button.addEventListener("click", function () {resetGame()})
     } else {
         console.log("ゲーム終了")
-        numTurn += 1
+        button.textContent = "ラウンド終了"
+        button.addEventListener("click", function () {console.log("ラウンド終了")})
     }
 }
 
@@ -214,7 +208,7 @@ async function p1_exchange() {
         return
     }
     // Select a new random element and replace the target card
-    const newElem = elements[Math.floor(Math.random() * elements.length)]
+    const newElem = elements[Math.floor(Math.random() * (elements.length-1))]
     p1_hand[targetElem] = newElem
     // Update the image element's appearance
     img.src = `../images/0.png`
@@ -274,8 +268,40 @@ function random_hand() {
     }
 }
 
+document.getElementById("generate_button").addEventListener("click", function () {
+    if (turn == "p2") {
+        time = "make"
+        done()
+    }
+})
+
+function resetGame() {
+    p1_hand = []; p2_hand = []; droped_cards_p1 = []; droped_cards_p2 = []; p1_selected_card = []; p2_selected_card = []
+    time = "game"
+    document.getElementById("p1_point").innerHTML = "ポイント："
+    document.getElementById("p1_explain").innerHTML = "　"
+    document.getElementById("p2_point").innerHTML = "ポイント："
+    document.getElementById("p2_explain").innerHTML = "　"
+    let p1_hand_element = document.getElementById("p1_hand")
+    while (p1_hand_element.firstChild) {p1_hand_element.removeChild(p1_hand_element.firstChild)}
+    let droped_area_p1_element = document.getElementById("droped_area_p1")
+    while (droped_area_p1_element.firstChild) {droped_area_p1_element.removeChild(droped_area_p1_element.firstChild)}
+    let p2_hand_element = document.getElementById("p2_hand")
+    while (p2_hand_element.firstChild) {p2_hand_element.removeChild(p2_hand_element.firstChild)}
+    let droped_area_p2_element = document.getElementById("droped_area_p2")
+    while (droped_area_p2_element.firstChild) {droped_area_p2_element.removeChild(droped_area_p2_element.firstChild)}
+    document.getElementById("generate_button").style.display = "inline"
+    document.getElementById("done_button").style.display = "none"
+    document.getElementById("nextButton").style.display = "none"
+    random_hand()
+    view_p1_hand()
+    view_p2_hand()
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     random_hand()
     view_p1_hand()
     view_p2_hand()
+    turn = Math.random()>=0.5 ? "p1" : "p2"
+    if (turn == "p1") { p1_exchange()}
 })
